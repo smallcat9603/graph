be1916dce8aad48e3db641140391e6eb98287379
smallcat9603 <huyao0107@gmail.com>
2023-02-20 17:52:50 +0900

    add neo4j aura

--- neo4j-igraph.py
+++ neo4j-igraph.py
@@ -1,7 +1,12 @@
 import py2neo as pg
 import igraph as ig
 
-neo4j = pg.Graph("bolt://54.237.250.148:7687", auth=("neo4j", "fifths-effectiveness-shafts"))
+# local
+# neo4j = pg.Graph("bolt://localhost:7687", auth=("neo4j", "j4oen"))
+# neo4j sandbox
+# neo4j = pg.Graph("bolt://3.237.242.164:7687", auth=("neo4j", "nomenclature-adhesive-removal"))
+# neo4j aura
+neo4j = pg.Graph("neo4j+s://3f3388c5.databases.neo4j.io", auth=("neo4j", "Zmf0G3eCjEWyxq6dbK6Q9h9PlHVJj_iFpojKkC0Mrps"))
 
 query = """
 LOAD CSV WITH HEADERS 
@@ -14,10 +19,11 @@ SET i.weight = toInteger(row.weight)
 """
 neo4j.run(query)
 
+# in the latest version of neo4j, size() should get a pattern comprehension as parameter, wrong: (c)--(), correct: [p=(c)--() | p]
 query = """
 MATCH (c:Character)
 RETURN c.name AS character,
-       size((c)--()) AS degree
+       size([p=(c)--() | p]) AS degree
 """
 data = neo4j.run(query).data() # dictlist
 print(data)
