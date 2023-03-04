f9c40e9c72f0bb7628ed57a2a0725c61a3f119c5
smallcat9603 <huyao0107@gmail.com>
2023-03-04 12:22:14 +0900

    machine learning

--- neo4j-link-prediction.py
+++ neo4j-link-prediction.py
@@ -81,7 +81,7 @@ plt.style.use('fivethirtyeight')
 ax = by_year.plot(kind='bar', x='year', y='count', legend=None, figsize=(15,8)) 
 ax.xaxis.set_label_text("") 
 plt.tight_layout() 
-plt.savefig("test.jpg")
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig1.jpg")
 # plt.show()
 
 query = """
@@ -232,7 +232,7 @@ for index, chart in enumerate(charts):
     histogram.plot(kind="bar", x='Common Authors', color="darkblue", ax=axs[index], title=f"Authors who {title} (label={label})")
     axs[index].xaxis.set_label_text("Common Authors")
 plt.tight_layout() 
-plt.savefig("hist.jpg")
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig2.jpg")
 # plt.show()
 
 def train_model(fields, training_data): 
@@ -243,3 +243,283 @@ def train_model(fields, training_data):
 basic_model = train_model(["commonAuthors"], training_data)
 eval_df = spark.createDataFrame( [(0,), (1,), (2,), (10,), (100,)], ['commonAuthors'])
 basic_model.transform(eval_df).select("commonAuthors", "probability", "prediction") .show(truncate=False)
+
+def evaluate_model(model, test_data): 
+    # Execute the model against the test set 
+    predictions = model.transform(test_data)
+    # Compute true positive, false positive, false negative counts 
+    tp = predictions[(predictions.label == 1) & (predictions.prediction == 1)].count()
+    fp = predictions[(predictions.label == 0) & (predictions.prediction == 1)].count()
+    fn = predictions[(predictions.label == 1) & (predictions.prediction == 0)].count()
+    # Compute recall and precision manually 
+    recall = float(tp) / (tp + fn) 
+    precision = float(tp) / (tp + fp)
+    # Compute accuracy using Spark MLLib's binary classification evaluator 
+    accuracy = BinaryClassificationEvaluator().evaluate(predictions)
+    # Compute false positive rate and true positive rate using sklearn functions 
+    labels = [row["label"] for row in predictions.select("label").collect()] 
+    preds = [row["probability"][1] for row in predictions.select ("probability").collect()]
+    fpr, tpr, threshold = roc_curve(labels, preds) 
+    roc_auc = auc(fpr, tpr)
+    return { "fpr": fpr, "tpr": tpr, "roc_auc": roc_auc, "accuracy": accuracy, "recall": recall, "precision": precision }
+
+def display_results(results): 
+    results = {k: v for k, v in results.items() if k not in ["fpr", "tpr", "roc_auc"]}
+    return pd.DataFrame({"Measure": list(results.keys()), "Score": list(results.values())})
+
+basic_results = evaluate_model(basic_model, test_data) 
+display_results(basic_results)
+
+def create_roc_plot(): 
+    plt.style.use('classic') 
+    fig = plt.figure(figsize=(13, 8)) 
+    plt.xlim([0, 1]) 
+    plt.ylim([0, 1]) 
+    plt.ylabel('True Positive Rate') 
+    plt.xlabel('False Positive Rate') 
+    plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'c', 'm', 'y', 'k'])))
+    plt.plot([0, 1], [0, 1], linestyle='--', label='Random score (AUC = 0.50)')
+    return plt, fig
+
+def add_curve(plt, title, fpr, tpr, roc): 
+    plt.plot(fpr, tpr, label=f"{title} (AUC = {roc:0.2})")
+
+plt, fig = create_roc_plot()
+add_curve(plt, "Common Authors", basic_results["fpr"], basic_results["tpr"], basic_results["roc_auc"])
+plt.legend(loc='lower right') 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig3.jpg")
+# plt.show()
+
+training_data.filter(training_data["label"]==1).describe().select("summary", "commonAuthors", "prefAttachment", "totalNeighbors").show()
+training_data.filter(training_data["label"]==0).describe().select("summary", "commonAuthors", "prefAttachment", "totalNeighbors").show()
+
+fields = ["commonAuthors", "prefAttachment", "totalNeighbors"] 
+graphy_model = train_model(fields, training_data)
+graphy_results = evaluate_model(graphy_model, test_data) 
+display_results(graphy_results)
+
+plt, fig = create_roc_plot()
+add_curve(plt, "Common Authors", basic_results["fpr"], basic_results["tpr"], basic_results["roc_auc"])
+add_curve(plt, "Graphy", graphy_results["fpr"], graphy_results["tpr"], graphy_results["roc_auc"])
+plt.legend(loc='lower right') 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig4.jpg")
+# plt.show()
+
+def plot_feature_importance(fields, feature_importances): 
+    feature_importances_new = [feature_importances[i] for i in range(len(feature_importances))]
+    df = pd.DataFrame({"Feature": fields, "Importance": feature_importances_new}) 
+    df = df.sort_values("Importance", ascending=False) 
+    ax = df.plot(kind='bar', x='Feature', y='Importance', legend=None) 
+    ax.xaxis.set_label_text("") 
+    plt.tight_layout() 
+    plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig5.jpg")
+    # plt.show()
+
+rf_model = graphy_model.stages[-1] 
+plot_feature_importance(fields, rf_model.featureImportances)
+
+###
+# 7. Predicting Links: Triangles and the Clustering Coefficient
+###
+
+query = """
+CALL gds.graph.drop("myGraph")
+"""
+graph.run(query)
+
+query = """
+CALL gds.graph.project(
+  'myGraph',    
+  ['Author'],   
+  {CO_AUTHOR_EARLY: {orientation: 'UNDIRECTED'}, CO_AUTHOR: {orientation: 'UNDIRECTED'}}    
+)
+"""
+graph.run(query)
+
+query = """
+CALL gds.triangleCount.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR_EARLY"],
+  writeProperty: 'trianglesTrain'
+})
+"""
+graph.run(query)
+
+query = """
+CALL gds.triangleCount.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR"],
+  writeProperty: 'trianglesTest'
+})
+"""
+graph.run(query)
+
+query = """
+CALL gds.localClusteringCoefficient.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR_EARLY"],
+  writeProperty: 'coefficientTrain'
+})
+"""
+graph.run(query)
+
+query = """
+CALL gds.localClusteringCoefficient.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR"],
+  writeProperty: 'coefficientTest'
+})
+"""
+graph.run(query)
+
+def apply_triangles_features(data, triangles_prop, coefficient_prop): 
+    query = """
+    UNWIND $pairs AS pair
+    MATCH (p1) WHERE id(p1) = pair.node1 
+    MATCH (p2) WHERE id(p2) = pair.node2 
+    RETURN pair.node1 AS node1, 
+           pair.node2 AS node2,
+           apoc.coll.min([p1[$trianglesProp], p2[$trianglesProp]]) AS minTriangles,
+           apoc.coll.max([p1[$trianglesProp], p2[$trianglesProp]]) AS maxTriangles,
+           apoc.coll.min([p1[$coefficientProp], p2[$coefficientProp]]) AS minCoefficient,
+           apoc.coll.max([p1[$coefficientProp], p2[$coefficientProp]]) AS maxCoefficient
+    """
+    params = { "pairs": [{"node1": row["node1"], "node2": row["node2"]} for row in data.collect()],
+               "trianglesProp": triangles_prop, 
+               "coefficientProp": coefficient_prop}
+    features = spark.createDataFrame(graph.run(query, params).to_data_frame()) 
+    return data.join(features, ["node1", "node2"])
+
+training_data = apply_triangles_features(training_data, "trianglesTrain", "coefficientTrain")
+test_data = apply_triangles_features(test_data, "trianglesTest", "coefficientTest")
+
+training_data.filter(training_data["label"]==1).describe().select("summary", "minTriangles", "maxTriangles", "minCoefficient", "maxCoefficient").show()
+training_data.filter(training_data["label"]==0).describe().select("summary", "minTriangles", "maxTriangles", "minCoefficient", "maxCoefficient").show()
+
+fields = ["commonAuthors", "prefAttachment", "totalNeighbors", "minTriangles", "maxTriangles", "minCoefficient", "maxCoefficient"]
+triangle_model = train_model(fields, training_data)
+triangle_results = evaluate_model(triangle_model, test_data) 
+display_results(triangle_results)
+
+plt, fig = create_roc_plot()
+add_curve(plt, "Common Authors", basic_results["fpr"], basic_results["tpr"], basic_results["roc_auc"])
+add_curve(plt, "Graphy", graphy_results["fpr"], graphy_results["tpr"], graphy_results["roc_auc"])
+add_curve(plt, "Triangles", triangle_results["fpr"], triangle_results["tpr"], triangle_results["roc_auc"])
+plt.legend(loc='lower right') 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig6.jpg")
+# plt.show()
+
+rf_model = triangle_model.stages[-1] 
+plot_feature_importance(fields, rf_model.featureImportances)
+
+###
+# 8. Predicting Links: Community Detection
+###
+
+query = """
+CALL gds.labelPropagation.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR_EARLY"],
+  writeProperty: 'partitionTrain'
+})
+"""
+graph.run(query)
+
+query = """
+CALL gds.labelPropagation.write("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR"],
+  writeProperty: 'partitionTest'
+})
+"""
+graph.run(query)
+
+query = """
+CALL gds.louvain.stream("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR_EARLY"],
+  includeIntermediateCommunities: true
+})
+YIELD nodeId, intermediateCommunityIds
+WITH gds.util.asNode(nodeId) AS node, intermediateCommunityIds[0] AS smallestCommunity
+SET node.louvainTrain = smallestCommunity
+"""
+graph.run(query)
+
+query = """
+CALL gds.louvain.stream("myGraph", {
+  nodeLabels: ["Author"],
+  relationshipTypes: ["CO_AUTHOR"],
+  includeIntermediateCommunities: true
+})
+YIELD nodeId, intermediateCommunityIds
+WITH gds.util.asNode(nodeId) AS node, intermediateCommunityIds[0] AS smallestCommunity
+SET node.louvainTest = smallestCommunity
+"""
+graph.run(query)
+
+def apply_community_features(data, partition_prop, louvain_prop): 
+    query = """
+    UNWIND $pairs AS pair
+    MATCH (p1) WHERE id(p1) = pair.node1 
+    MATCH (p2) WHERE id(p2) = pair.node2 
+    RETURN pair.node1 AS node1, 
+           pair.node2 AS node2,
+           CASE WHEN p1[$partitionProp] = p2[$partitionProp] THEN 1 ELSE 0 END AS samePartition,
+           CASE WHEN p1[$louvainProp] = p2[$louvainProp] THEN 1 ELSE 0 END AS sameLouvain
+    """
+    params = { "pairs": [{"node1": row["node1"], "node2": row["node2"]} for row in data.collect()],
+               "partitionProp": partition_prop, 
+               "louvainProp": louvain_prop
+    }
+    features = spark.createDataFrame(graph.run(query, params).to_data_frame()) 
+    return data.join(features, ["node1", "node2"])
+
+training_data = apply_community_features(training_data, "partitionTrain", "louvainTrain")
+test_data = apply_community_features(test_data, "partitionTest", "louvainTest")
+
+plt.style.use('fivethirtyeight') 
+fig, axs = plt.subplots(1, 2, figsize=(18, 7), sharey=True) 
+charts = [(1, "have collaborated"), (0, "haven't collaborated")]
+
+for index, chart in enumerate(charts): 
+    label, title = chart
+    filtered = training_data.filter(training_data["label"] == label) 
+    values = (filtered.withColumn('samePartition', F.when(F.col("samePartition") == 0, "False").otherwise("True")).groupby("samePartition").agg(F.count("label").alias("count")).select("samePartition", "count").toPandas())
+    values.set_index("samePartition", drop=True, inplace=True) 
+    values.plot(kind="bar", ax=axs[index], legend=None, title=f"Authors who {title} (label={label})")
+    axs[index].xaxis.set_label_text("Same Partition")
+
+plt.tight_layout() 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig7.jpg")
+# plt.show()
+
+for index, chart in enumerate(charts): 
+    label, title = chart 
+    filtered = training_data.filter(training_data["label"] == label) 
+    values = (filtered.withColumn('sameLouvain', F.when(F.col("sameLouvain") == 0, "False").otherwise("True")).groupby("sameLouvain").agg(F.count("label").alias("count")).select("sameLouvain", "count") .toPandas())
+    values.set_index("sameLouvain", drop=True, inplace=True) 
+    values.plot(kind="bar", ax=axs[index], legend=None, title=f"Authors who {title} (label={label})")
+    axs[index].xaxis.set_label_text("Same Louvain")
+
+plt.tight_layout() 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig8.jpg")
+# plt.show()
+
+fields = ["commonAuthors", "prefAttachment", "totalNeighbors", "minTriangles", "maxTriangles", "minCoefficient", "maxCoefficient", "samePartition", "sameLouvain"]
+community_model = train_model(fields, training_data)
+
+community_results = evaluate_model(community_model, test_data) 
+display_results(community_results)
+
+plt, fig = create_roc_plot()
+add_curve(plt, "Common Authors", basic_results["fpr"], basic_results["tpr"], basic_results["roc_auc"])
+add_curve(plt, "Graphy", graphy_results["fpr"], graphy_results["tpr"], graphy_results["roc_auc"])
+add_curve(plt, "Triangles", triangle_results["fpr"], triangle_results["tpr"], triangle_results["roc_auc"])
+add_curve(plt, "Community", community_results["fpr"], community_results["tpr"], community_results["roc_auc"])
+plt.legend(loc='lower right') 
+plt.savefig("/Users/smallcat/Documents/GitHub/graph/fig9.jpg")
+# plt.show()
+
+rf_model = community_model.stages[-1] 
+plot_feature_importance(fields, rf_model.featureImportances)
