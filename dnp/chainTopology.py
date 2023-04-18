be080c19477e9ec531dd0724435094bfab5d6e76
smallcat9603 <huyao0107@gmail.com>
2023-04-18 13:54:01 +0900

    add pyro4 chain example

---
+++ dnp/chainTopology.py
@@ -0,0 +1,21 @@
+# from __future__ import print_function
+import Pyro4
+class Chain(object):
+    def __init__(self, name, next):
+        self.name = name
+        self.nextName = next
+        self.next = None
+    @Pyro4.expose
+    def process(self, message):
+        if self.next is None:
+            self.next = Pyro4.core.Proxy("PYRONAME:example.chainTopology." + self.nextName)
+        if self.name in message:
+            print("Back at %s; the chain is closed!" % self.name)
+            return ["complete at " + self.name]
+        else:
+            print("%s forwarding the message to the object %s" \
+                  % (self.name, self.nextName))
+            message.append(self.name)
+            result = self.next.process(message)
+            result.insert(0, "passed on from " + self.name)
+            return result
---
+++ dnp/chain_client.py
@@ -0,0 +1,4 @@
+# from __future__ import print_function
+import Pyro4
+obj = Pyro4.core.Proxy("PYRONAME:example.chainTopology.1")
+print("Result=%s" % obj.process(["hello"]))
