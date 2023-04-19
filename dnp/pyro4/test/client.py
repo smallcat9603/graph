1c95b02db1dc54fbcbb479cafa01a5641ca5bffb
smallcat9603 <huyao0107@gmail.com>
2023-04-19 12:47:54 +0900

    minor

--- dnp/pyro4/test/client.py
+++ dnp/pyro4/test/client.py
@@ -1,18 +1,18 @@
 import Pyro4
 
-# ### option 1
-# uri = input("What is the Pyro uri of the greeting object? ").strip()
+# ### option 1 (require server ip)
+# uri = input("What is the Pyro uri of the greeting object? ").strip() # e.g., "PYRO:obj_7f2e746b12934b55a119b9de946cf6e7@0.0.0.0:56739"
 
-# ### option 2
+# ### option 2 (name server is local)
 # uri = "PYRONAME:server"
 
-# ### option 3
+# ### option 3 (name server is local)
 # uri = Pyro4.locateNS().lookup("server") 
 
-# ### option 4
+# ### option 4 (require name server ip)
 # uri = Pyro4.locateNS(host="127.0.0.1").lookup("server") # ns default port = 9090
 
-### option 5
+### option 5 (require name server ip)
 uri = "PYRONAME:server@127.0.0.1" # "PYRONAME:server@127.0.0.1:9090"
 
 
