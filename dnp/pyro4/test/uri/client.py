397966d0b6378c62aff621e9d17224bc52815b1e
smallcat9603 <huyao0107@gmail.com>
2023-04-21 14:16:36 +0900

    remote connection via ip address

--- dnp/pyro4/test/uri/client.py
+++ dnp/pyro4/test/uri/client.py
@@ -9,14 +9,14 @@ import Pyro4
 # ### option 3 (name server is local)
 # uri = Pyro4.locateNS().lookup("server") 
 
-# ### option 4 (require name server ip)
+# ### option 4 (require local name server ip)
 # uri = Pyro4.locateNS(host="127.0.0.1").lookup("server") # ns default port = 9090
 
-# ### option 5 (require name server ip)
+# ### option 5 (require local name server ip)
 # uri = "PYRONAME:server@127.0.0.1" # "PYRONAME:server@127.0.0.1:9090"
 
 ### option 6 (remote connection via ip address)
-uri = "PYRONAME:server@192.168.3.39" # name server ip
+uri = "PYRONAME:server@192.168.3.39" # name server ip, default port = 9090
 
 
 server = Pyro4.Proxy(uri)
