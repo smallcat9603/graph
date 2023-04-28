2a6dc84f4d2582c9330a60d69470850a785bec23
smallcat9603 <huyao0107@gmail.com>
2023-04-28 23:53:54 +0900

    rw v0

--- dnp/pyro4/rw/v0/rw.py
+++ dnp/pyro4/rw/v0/rw.py
@@ -1,32 +1,61 @@
 import Pyro4
 import random
-
-def generate_random_numbers():
-    num_of_numbers = random.randint(1, 4)
-    random_numbers = []
-    for i in range(num_of_numbers):
-        random_numbers.append(random.randint(1, 100))
-    return random_numbers
+import igraph as ig
+import sys
 
 class Walker(object):
-    def __init__(self, name, next):
+    def __init__(self, name, graph, route_table, node_map, nhops): 
         self.name = name
-        self.nextName = next
+        self.graph = graph
+        self.route_table = route_table
+        self.node_map = node_map # node_map = {100: 0, 200: 1, 150: 2, ...}, global --> local
+        self.map_node = {v: k for k, v in node_map.items()} # reverse keys and values in node_map, map_node = {0: 100, 1: 200, 2: 150, ...}, local --> global
+        self.nhops = nhops
         self.next = None
+    def nexthop(self, cur_local, cur_global):
+        neighbors_in = self.graph.vs[cur_local].neighbors()
+        nneighbors_in = len(neighbors_in)
+        neighbors_out = self.route_table[cur_global]
+        nneighbors_out = len(neighbors_out)
+        nneighbors = nneighbors_in + nneighbors_out
+        next_idx = random.randint(nneighbors-1)
+        next_local_node = -1
+        next_global_node = -1
+        next_global_server = -1
+        if next_idx < nneighbors_in: # next node is inside server
+            next_local_node = neighbors_in[next_idx]
+            next_global_node = self.map_node[next_local_node]
+        else: # next node is outside server
+            next_global = neighbors_out[next_idx-nneighbors_in]
+            next_global_node = next_global[0]
+            next_global_server = next_global[1]   
+        return next_local_node, next_global_node, next_global_server
     @Pyro4.expose
-    def walk(self, message):
-        if self.next is None:
-            self.next = Pyro4.Proxy("PYRONAME:Server" + self.nextName)
-        if self.name in message:
-            rn = generate_random_numbers()
-            print("Server{0} walks through nodes: {1}".format(self.name, rn))
-            print("Walker stopped at server%s" % self.name)
-            return rn
+    def walk(self, message): 
+        next_local_node = -1
+        next_global_node = -1
+        next_global_server = -1
+        if message[-1] == "go":
+            start_local = random.randint(self.graph.vcount()-1)
+            start_global = self.map_node[start_local]
+            next_local_node, next_global_node, next_global_server = self.nexthop(start_local, start_global)
+            message = [start_global]
+            message.append(next_global_node)
         else:
-            message.append(self.name)
-            result = self.next.walk(message)
-            rn = generate_random_numbers()
-            print("Server{0} walks through nodes: {1}".format(self.name, rn))
-            result = rn + result
-            print("Walker walks from server%s to server%s" % (self.name, self.nextName))
-            return result
+            while next_global_server == -1 and len(message) < self.nhops:
+                cur_global = message[-1]
+                cur_local = self.node_map[cur_global]
+                next_local_node, next_global_node, next_global_server = self.nexthop(cur_local, cur_global)
+                message.append(next_global_node)
+            if len(message) >= self.nhops:
+                print("Walker stopped at Server{0}".format(self.name))
+                print("Walker walks through nodes: {0}".format(message))
+            elif next_local_node == -1:
+                print("Walker walks through nodes: {0}".format(message))
+                nextname = str(next_global_server)
+                print("Walker walks from Server{0} to Server{1}".format(self.name, nextname))
+                self.next = Pyro4.Proxy("PYRONAME:Server" + nextname)
+                self.next.walk(message)
+            else:
+                print("Something is wrong")
+                sys.exit(1)
--- dnp/pyro4/rw/v0/server.py
+++ dnp/pyro4/rw/v0/server.py
@@ -1,18 +1,53 @@
 import Pyro4
-import rw
 import sys
+import igraph as ig
+import pandas as pd
+import rw
+
+# map to node ids that are from 0 and continuous
+def map_nodes_in_edgelist(file, file_new):
+    node_map = {}
+    with open(file, "r") as f:
+        edgelist = [line.strip().split() for line in f]
+        nodes = set([edge[0] for edge in edgelist] + [edge[1] for edge in edgelist])
+        node_map = {node: i for i, node in enumerate(nodes)}
+        edgelist_new = [(node_map[edge[0]], node_map[edge[1]]) for edge in edgelist]
+        df = pd.DataFrame(edgelist_new)
+        df.to_csv(file_new, sep=" ", index=False, header=False)
+        return node_map
 
 nservers = 3 # server id = 0,1,2
+this = sys.argv[1]
 
-if len(sys.argv) != 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) > nservers-1 or int(sys.argv[1]) < 0:
+if len(sys.argv) != 2 or not this.isdigit() or int(this) > nservers-1 or int(this) < 0:
     print("input server id [0-{0}]".format(nservers-1))
     sys.exit(1)
 
-this = sys.argv[1]
-next = str(int(sys.argv[1])%nservers + 1)
+# standardize node id in edgelist file and read subgraph, otherwise graph is not connected in igraph
+file = f"test.sub{this}.txt"
+file_new = file.rstrip(".txt") + ".x.txt"
+node_map = map_nodes_in_edgelist(file, file_new)
+graph = ig.Graph.Read_Edgelist(file_new, directed=False)
+
+# check graph
+if(graph.is_connected()):
+    print("Graph is connected")
+    print(graph.summary())
+else:
+    print("Graph is not connected")
+    sys.exit(1)
+
+# read route table from file
+columns = ["sources", "targets_servers"]
+routes = pd.read_csv(f"test.rt{this}.txt", comment="#", sep="\s+", names=columns)
+route_table = {}
+for row in range(len(routes)):
+    route_table[routes["sources"][row]] = eval(routes["targets_servers"][row])
+
 servername = "Server" + this
 daemon = Pyro4.Daemon()
-obj = rw.Walker(this, next)
+nhops = 100
+obj = rw.Walker(this, graph, route_table, node_map, nhops)
 uri = daemon.register(obj)
 ns = Pyro4.locateNS()
 ns.register(servername, uri)
