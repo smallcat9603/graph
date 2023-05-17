9e17db93ac6dd62bc73caaf553e97aef574a6cf7
smallcat9603 <huyao0107@gmail.com>
2023-05-17 16:15:25 +0900

    partion git graph to 3 subgraphs

---
+++ tools/csv_to_edgelist.py
@@ -0,0 +1,40 @@
+# smallcat 230517
+
+import igraph as ig
+import pandas as pd
+import sys, getopt, os
+
+def printUsage():
+    print('Usage: python3 {0} <edgelistfile>'.format(os.path.basename(__file__)))
+
+def main(argv):
+    index_col = None
+    header = None
+    try:
+        opts, args = getopt.getopt(argv, "hIH") # opts = [("-h", " "), ("-I", " "), ("-H", " ")], args = [edgelistfile]
+    except getopt.GetoptError:
+        printUsage()
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt == '-h':
+            printUsage()
+            sys.exit()
+        elif opt == '-I':
+            index_col = 0    
+        elif opt == '-H':
+            header = 0
+        else:
+            printUsage()
+            sys.exit(1)
+    if len(args) != 1:
+            printUsage()
+            sys.exit(1)       
+    edgefile = args[0]
+
+    data = pd.read_csv(edgefile, index_col=index_col, header=header)
+    txt = edgefile.rstrip(".csv") + ".txt"
+    data.to_csv(txt, sep='\t', index=False, header=False)
+    print(txt + " generated.")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])  
