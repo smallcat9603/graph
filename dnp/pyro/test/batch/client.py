4d3207abb46fe4ab34c240622d6f85b8c12f8327
smallcat9603 <huyao0107@gmail.com>
2023-05-31 13:13:12 +0900

    minor

--- dnp/pyro/test/batch/client.py
+++ dnp/pyro/test/batch/client.py
@@ -15,13 +15,14 @@ if system == "Linux":
 uri = "PYRO:server@" + host + ":9091"
 start = time.time()
 obj = Pyro5.client.Proxy(uri)
-batch = Pyro5.api.BatchProxy(obj)
-for i in range(1000):
-    try:
-        batch.run()
-    except Exception:
-        print("Pyro traceback:")
-        print("".join(Pyro5.errors.get_pyro_traceback()))
-batch()
+# batch = Pyro5.api.BatchProxy(obj)
+# for i in range(1000):
+try:
+    # batch.run()
+    obj.start(1000)
+except Exception:
+    print("Pyro traceback:")
+    print("".join(Pyro5.errors.get_pyro_traceback()))
+# batch()
 end = time.time()
 print(f"time = {end-start}")
