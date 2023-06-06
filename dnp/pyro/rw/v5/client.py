6ca9a9153f452178e046f0b26c05d2b1558e68e4
smallcat9603 <huyao0107@gmail.com>
2023-06-06 17:31:41 +0900

    start to implement multiprocessing

---
+++ dnp/pyro/rw/v5/client.py
@@ -0,0 +1,154 @@
+import Pyro5
+import Pyro5.client
+import Pyro5.api
+import sys, getopt, os, platform
+import pandas as pd
+import time
+import threading
+import json
+
+def printUsage():
+    print('Usage: python3 {0} -w [nwalkers] -s [nsteps] -t [timestep] <number_of_servers>'.format(os.path.basename(__file__)))
+
+def count_finished(timestep, hosts, nhosts, Nwalkers):
+    global start_times
+    global stop_times
+    global go_outs
+    global merged_paths
+    global finished
+    global nprocesses
+    global max_threads
+    global chunk_sizes
+    finished = []
+    while True:
+        start_times = [] 
+        stop_times = [] 
+        go_outs = []
+        merged_paths = [] # use list instead of dict to avoid "RuntimeError: dictionary changed size during iteration" error
+        nprocesses = []
+        max_threads = []
+        chunk_sizes = []
+        for host in range(nhosts):
+            ip = hosts[host]
+            uri = "PYRO:walker@" + ip
+            obj = Pyro5.client.Proxy(uri) # connect to server directly (not need ns anymore)
+            try:
+                starttime, stoptime, goout, paths, processes, maxthreads, chunksize = obj.get_results()
+                start_times.append(starttime)
+                stop_times.append(stoptime)
+                go_outs.append(goout)
+                merged_paths += paths
+                nprocesses.append(processes)
+                max_threads.append(maxthreads)
+                chunk_sizes.append(chunksize)
+            except Exception:
+                print(f"Pyro traceback:\n{''.join(Pyro5.errors.get_pyro_traceback())}")  
+        finished.append(len(merged_paths))
+        print(finished[-1], end="\t", flush=True)
+        if finished[-1] == Nwalkers or (len(finished) > 7 and finished[-1]-finished[-8] < 1):
+            break
+        time.sleep(timestep)
+
+def start_server(uri, nhops, id_start, id_end):
+    # This will run in a thread. Create a proxy just for this thread:
+    with Pyro5.client.Proxy(uri) as p:
+        try:
+            p.start_walkers(nhops, id_start, id_end)
+        except Exception:
+            print(f"Pyro traceback:\n{''.join(Pyro5.errors.get_pyro_traceback())}")           
+
+def main(argv):
+    nwalkers = 1 # number of walkers starting from each server
+    nhops = 80 # path length for each walker
+    timestep = 1
+
+    try:
+        opts, args = getopt.getopt(argv, "hw:s:t:") 
+    except getopt.GetoptError:
+        printUsage()
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt == '-h':
+            printUsage()
+            sys.exit()
+        elif opt == '-w':
+            nwalkers = int(arg)
+        elif opt == '-s':
+            nhops = int(arg)
+        elif opt == '-t':
+            timestep = int(arg)            
+        else:
+            printUsage()
+            sys.exit(1)
+    nhosts = 3
+    if len(args) == 1 and 1 <= int(args[0]) <= 8:
+        nhosts = int(args[0])   
+    else:        
+        print("input number of servers (<= 8)")
+        sys.exit(1)      
+
+    # match hostfile
+    filename = ""
+    uname = platform.uname()
+    system = uname[0]
+    node = uname[1]
+    if system == "Darwin":
+        filename = "hosts_local.txt"
+    elif system == "Linux":
+        if "calc" in node:
+            filename = "hosts_calc.txt"
+
+    # read hosts from file
+    columns = ["server_id", "ip_port"]
+    hostfile = pd.read_csv(filename, comment="#", sep="\s+", names=columns)
+    hosts = {}
+    for row in range(nhosts):
+        hosts[int(hostfile["server_id"][row])] = hostfile["ip_port"][row]  
+
+    # start servers
+    for host in range(nhosts):
+        id_start = host * nwalkers
+        id_end = id_start + nwalkers
+        ip = hosts[host]
+        uri = "PYRO:walker@" + ip
+        print(f"Client starts {nwalkers} Walkers[{id_start}-{id_end-1}] at Server{host} ({ip}) ...")
+        t = threading.Thread(target=start_server, args=[uri, nhops, id_start, id_end])
+        t.daemon = True
+        t.start()
+          
+    # total number of walkers = nhosts * nwalkers
+    Nwalkers = nhosts*nwalkers
+    t_cf = threading.Thread(target=count_finished, args=[timestep, hosts, nhosts, Nwalkers])
+    t_cf.daemon = True
+    t_cf.start() 
+    t_cf.join()
+    print("Done.")
+
+    # output results
+    runtime = max(stop_times)-max(start_times)
+    print(f"time = {runtime}")
+    print(f"goout = {sum(go_outs)}")
+    timestamp = int(max(start_times))
+    dir = "../log"
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    processes = max(nprocesses)
+    maxthreads = max(max_threads)
+    chunksize = max(chunk_sizes)
+    jsonfile = dir + f"/{timestamp}_n{nhosts}_p{processes}_t{maxthreads}_c{chunksize}.json"
+    txtfile = dir + f"/{timestamp}_n{nhosts}_p{processes}_t{maxthreads}_c{chunksize}.txt"
+    with open(jsonfile, 'w') as file:
+        json.dump(dict(merged_paths), file)
+    print(f"paths saved in {jsonfile}")
+    line = f"{runtime}\t{len(merged_paths)}\t{sum(go_outs)}"
+    for each in go_outs:
+        line += f"\t{each}"
+    line += "\n"
+    with open(txtfile, 'w') as file:
+        file.write(line)
+        for item in finished:
+            file.write(str(item) + '\n')
+    print(f"statistics saved in {txtfile}")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ dnp/pyro/rw/v5/rw.py
@@ -0,0 +1,125 @@
+import Pyro5.server
+import Pyro5.client
+import random
+import sys
+import time
+import concurrent.futures
+import multiprocessing
+
+# @Pyro5.server.behavior(instance_mode="single")
+class Walker(object):
+    def __init__(self, name, graph, nprocesses, max_threads, chunk_size, route_table, node_map, hosts): # nodes in route_table are global
+        self.name = name
+        self.graph = graph
+        self.nprocesses = nprocesses
+        self.max_threads = max_threads
+        self.chunk_size = chunk_size
+        self.route_table = route_table
+        self.node_map = node_map # node_map = {100: 0, 200: 1, 150: 2, ...}, global --> local
+        self.map_node = {v: k for k, v in node_map.items()} # reverse keys and values in node_map, map_node = {0: 100, 1: 200, 2: 150, ...}, local --> global
+        self.hosts = hosts
+        self.nhosts = len(hosts)
+        self.start_time = 0.0
+        self.stop_time = 0.0
+        self.paths = []
+        self.go_out = 0
+
+    def nexthop_roulette(self, cur_local, cur_global):
+        neighbors_in = self.graph.neighbors(cur_local)
+        nneighbors_in = len(neighbors_in)
+        nneighbors_out = 0
+        if cur_global in self.route_table:
+            neighbors_out = self.route_table[cur_global]
+            nneighbors_out = len(neighbors_out)
+        nneighbors = nneighbors_in + nneighbors_out
+        next_idx = random.randint(0, nneighbors-1)
+        next_local_node = -1
+        next_global_node = -1
+        next_global_server = -1
+        if next_idx < nneighbors_in: # next node is inside server
+            next_local_node = neighbors_in[next_idx]
+            next_global_node = self.map_node[next_local_node]
+        else: # next node is outside server
+            next_global = neighbors_out[next_idx-nneighbors_in]
+            next_global_node = next_global[0]
+            next_global_server = next_global[1]   
+        return next_local_node, next_global_node, next_global_server
+
+    def remote_invoke(self, next_global_server, message, nhops, walker):
+        # nextname = str(next_global_server)
+        # print(f"Walker{walker} walked through {len(message)} nodes, and will go from Server{self.name} to Server{nextname}")
+        uri = "PYRO:walker@" + self.hosts[next_global_server]
+        # with Pyro5.client.Proxy("PYRONAME:Server" + nextname) as next: # require ns
+        with Pyro5.client.Proxy(uri) as next: # not require ns
+            next.walk(message, nhops, walker)
+            # try:
+            #     next.walk(message, nhops, walker)
+            # except Pyro5.errors.ConnectionClosedError:  # or possibly CommunicationError
+            #     print(f"Connection lost. REBINDING to Server{next_global_server} ({self.hosts[next_global_server]})...")
+            #     next._pyroReconnect()
+
+    @Pyro5.server.expose
+    def get_results(self):
+        return self.start_time, self.stop_time, self.go_out, self.paths, self.nprocesses, self.max_threads, self.chunk_size
+
+    @Pyro5.server.expose
+    # @Pyro5.server.oneway
+    def walk(self, message, nhops, walker): 
+        next_local_node = -1
+        next_global_node = -1
+        next_global_server = -1
+        while next_global_server == -1 and len(message) < nhops: # walk inside
+            msg = message[-1]
+            if isinstance(msg, str) and msg == "go": # starting point of walker
+                print(f"Walker{walker} gets started to walk at Server{self.name}")
+                cur_local = random.randint(0, self.graph.vcount()-1)
+                cur_global = self.map_node[cur_local]
+                message = [cur_global]   
+            elif isinstance(msg, int) and msg >= 0:
+                cur_global = msg
+                cur_local = self.node_map[cur_global]
+            else:
+                print("messge is wrong")
+                sys.exit(1)
+            next_local_node, next_global_node, next_global_server = self.nexthop_roulette(cur_local, cur_global)
+            message.append(next_global_node)                      
+        if len(message) >= nhops:
+            print(f"Finished. Walker{walker} stopped at Server{self.name}, walking through {len(message)} nodes.")
+            self.stop_time = time.time()
+            self.paths.append((walker, message))
+        elif next_local_node == -1: # walk outside
+            self.go_out += 1
+            self.remote_invoke(next_global_server, message, nhops, walker)
+        else:
+            print(f"Something is wrong for Walker{walker}")
+            sys.exit(1)
+
+    @Pyro5.server.expose
+    # @Pyro5.server.oneway
+    def start_walkers(self, nhops, id_start, id_end): 
+        self.start_time = time.time()
+        time.sleep(0.001) # prevent arriving before starting
+        print(f"Walkers[{id_start}-{id_end-1}] start at Server{self.name} ...")
+
+        # # option1: stupid max_thread_num control by delay
+        # for walker in range(id_start, id_end):
+        #     t = threading.Thread(target=self.walk, args=[["go"], nhops, walker])
+        #     t.daemon = True
+        #     t.start()
+        #     time.sleep(0.3) # avoid Pyro5.errors.ConnectionClosedError (m1 experience: 1-server --> 0.05, 2-server --> 0.1, 4-server --> 0.3)
+
+        # option2: automatic max_thread_num control by ThreadPool submit
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_threads) as executor:
+        #     for walker in range(id_start, id_end):
+        #         executor.submit(self.walk, ["go"], nhops, walker)
+        #     # executor.shutdown()
+
+        # # option3: automatic max_thread_num control by ThreadPool map
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_threads) as executor:
+        #     params = map(lambda x: (["go"], nhops, x), range(id_start, id_end))
+        #     executor.map(lambda args: self.walk(*args), params, chunksize=self.chunk_size)
+
+        # option4: multiprocessing map
+        with multiprocessing.Pool(processes=self.nprocesses) as pool:
+            params = map(lambda x: (["go"], nhops, x), range(id_start, id_end))
+            pool.starmap(self.walk, params, chunksize=self.chunk_size)
