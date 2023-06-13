d013b3a22f73e39498f11a5768e74af58ccecc14
smallcat9603 <huyao0107@gmail.com>
2023-06-13 15:14:11 +0900

    use MPI_Iprobe to check data arriving

--- dnp/mpi/rw/rw.c
+++ dnp/mpi/rw/rw.c
@@ -13,22 +13,12 @@
 
 int main(int argc, char** argv) {
 
-  // Initialize the MPI environment
   MPI_Init(NULL, NULL);
-  // Find out rank, size
+
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int size;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  // We are assuming at least 2 processes for this task
-  if (size != 2) {
-    fprintf(stderr, "World size must be two for %s\n", argv[0]);
-    MPI_Abort(MPI_COMM_WORLD, 1);
-  }
-
-  int received_flag = 0;
-  int send_flag = 0;
 
   double start_time, end_time, last_received_time;
   start_time = MPI_Wtime();
@@ -38,28 +28,32 @@ int main(int argc, char** argv) {
   send[0] = rank;
   send[1] = rank + 1;
   int partner_rank = (rank+1)%2;
-  int count = 1;
+  int count = 2;
 
-  while (MPI_Wtime() - last_received_time < 1) {
+  MPI_Status status;
+  int flag = 0;
 
-    MPI_Request req_recv;
-    MPI_Status st;
-    int* recv = (int*) malloc(sizeof(int)*2);
-    MPI_Irecv(recv, 100, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req_recv);
-    while (count > 0){
-      MPI_Request req_send;
-      MPI_Isend(send, 2, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, &req_send);  
-      count--;
-    }
-    while(!received_flag){
-      MPI_Test(&req_recv, &received_flag, &st);
-      if (received_flag) {
-        int received_count;
-        MPI_Get_count(&st, MPI_INT, &received_count);
-        printf("rank = %d, received_count=%d\n", rank, received_count);
-        last_received_time = MPI_Wtime();
-      }
+  while (count > 0){
+  MPI_Request req;
+  MPI_Isend(send, 2, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, &req);  
+  count--;
+  }
+
+  while (MPI_Wtime() - last_received_time < 3) {
+
+    MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+    if (flag) {
+      int received_count;
+      MPI_Get_count(&status, MPI_INT, &received_count);
+      printf("rank = %d, received_count=%d\n", rank, received_count);
+      last_received_time = MPI_Wtime();
+      MPI_Request req_recv;
+      MPI_Status st;
+      int* recv = (int*) malloc(sizeof(int)*2);
+      MPI_Irecv(recv, received_count, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req_recv);
+      printf("rank=%d, recv[0]=%d, recv[1]=%d\n", rank, recv[0], recv[1]);
     } 
+
   }
 
   end_time = MPI_Wtime();
