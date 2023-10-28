4d9562fbd28043b6f966947899ce10a4689a0f9f
smallcat <huyao0107@gmail.com>
2023-10-28 12:14:12 +0900

    data to json

--- dnp/kg/data_to_json.py
+++ dnp/kg/data_to_json.py
@@ -1,6 +1,6 @@
 # smallcat 231027
 #
-# transform data in data/ to json format
+# transform data in dir/ to json format
 
 import pandas as pd
 import json
@@ -26,51 +26,61 @@ def main(argv):
             printUsage()
             sys.exit(1)       
 
-    files = os.listdir("data")
-    files_with_out = [file for file in files if "_out.txt" in file]
-    files_with_tfidf = [file for file in files if "_tfidf.txt" in file]
+    dir = "data"
+    files = os.listdir(dir)
+    files_out = [file for file in files if "_out.txt" in file]
+    files_tfidf = [file for file in files if "_tfidf.txt" in file]
     
     result = {}
+    blacklist = []
 
-    for input_file in files_with_out:
-        with open(input_file, 'r') as file:
+    for input_file in files_out:
+        with open(dir+"/"+input_file, 'r') as file:
             for line in file:
                 line = eval(line)
+                length = len(line)
+                if length < 2:
+                    blacklist.append(line[0])
+                    continue
                 newKey = False
                 if line[0] not in result:
                     elements = line[1].split("-")
-                    result[line[0]] = {elements[0]:{elements[1]:{"lines": [elements[2]]}}}  
+                    result[line[0]] = {elements[0]:{elements[1]:{"lines": [int(elements[2])]}}}  
                     newKey = True              
-                for i in range(1, len(line)):
+                for i in range(1, length):
                     if i == 1 and newKey == True:
                         continue
                     elements = line[i].split("-")
                     if elements[0] not in result[line[0]]:
-                        result[line[0]][elements[0]] = {elements[1]:{"lines": [elements[2]]}}
+                        result[line[0]][elements[0]] = {elements[1]:{"lines": [int(elements[2])]}}
                     elif elements[1] not in result[line[0]][elements[0]]:
-                        result[line[0]][elements[0]][elements[1]] = {"lines": [elements[2]]}
-                    elif elements[2] not in result[line[0]][elements[0]][elements[1]]["lines"]:
-                        result[line[0]][elements[0]][elements[1]]["lines"].append(elements[2])
+                        result[line[0]][elements[0]][elements[1]] = {"lines": [int(elements[2])]}
+                    elif int(elements[2]) not in result[line[0]][elements[0]][elements[1]]["lines"]:
+                        result[line[0]][elements[0]][elements[1]]["lines"].append(int(elements[2]))
 
-    for input_file in files_with_tfidf:
-        with open(input_file, 'r') as file: 
+    for input_file in files_tfidf:
+        with open(dir+"/"+input_file, 'r') as file: 
             ref = []
             for line in file:
                 line = eval(line)
-                if line[0] not in result:
+                length = len(line)
+                if line[0] in blacklist:
+                    continue
+                elif line[0] not in result:
                     ref = line
                 else:
-                    for i in (1, len(line)):
+                    for i in range(1, length):
                         if float(line[i]) > 0:
                             element0 = ref[i].split("-")[0]
                             element1 = ref[i].split("-")[1].split(".")[0]
-                            if "tfidf" not in result[line[0]][element0][element1]:
+                            # print(line[0], " ", element0, " ", element1)
+                            if element0 in result[line[0]] and element1 in result[line[0]][element0] and "tfidf" not in result[line[0]][element0][element1]:
                                 result[line[0]][element0][element1]["tfidf"] = float(line[i])
                             
-    with open('data.json', 'w') as json_file:
-        json.dump(result, json_file, indent=4)
+    with open(dir+"/"+'data.json', 'w') as json_file:
+        json.dump(result, json_file, indent=4, ensure_ascii=False)
 
-    print("saved in data.json")
+    print("saved in " + dir + "/data.json")
 
 
 if __name__ == "__main__":
