22b2d78b413bd17abf6a1d1a9fa73725e26ba80e
smallcat9603 <huyao0107@gmail.com>
2024-01-05 22:24:21 +0900

    minor

--- dnp/kg/llm/test.py
+++ dnp/kg/llm/test.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 
-llm = OpenAI(openai_api_key="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")
+llm = OpenAI(openai_api_key="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") # key no longer available after uploaded to github
 
 response = llm("What is Neo4j?")
 
