66f15c8803faa82c78b2c9e6f967dfabb0e013f8
smallcat9603 <huyao0107@gmail.com>
2024-01-10 23:02:51 +0900

    Revert "minor"
    This reverts commit 8c0d81ca7f3a904ae95756282c2fb2576166e605.

--- dnp/kg/pages/4_bot.py
+++ dnp/kg/pages/4_bot.py
@@ -54,10 +54,54 @@ url=st.secrets["NEO4J_LOCAL"]
 username=st.secrets["NEO4J_LOCAL_USER"]
 password=st.secrets["NEO4J_LOCAL_PASSWORD"]
 graph = Neo4jGraph(
+    # url=st.secrets["NEO4J_URI"],
+    # username=st.secrets["NEO4J_USERNAME"],
+    # password=st.secrets["NEO4J_PASSWORD"],
     url=url,
     username=username,
     password=password,
 )
+
+CYPHER_GENERATION_TEMPLATE = """
+You are an expert Neo4j Developer translating user questions into Cypher to answer questions about movies and provide recommendations.
+Convert the user's question based on the schema.
+
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+
+Fine Tuning:
+
+For movie titles that begin with "The", move "the" to the end. For example "The 39 Steps" becomes "39 Steps, The" or "the matrix" becomes "Matrix, The".
+
+Example Cypher Statements:
+
+1. How to find how many degrees of separation there are between two people:
+```
+MATCH path = shortestPath(
+  (p1:Person {{name: "Actor 1"}})-[:ACTED_IN|DIRECTED*]-(p2:Person {{name: "Actor 2"}})
+)
+WITH path, p1, p2, relationships(path) AS rels
+RETURN
+  p1 {{ .name, .born, link:'https://www.themoviedb.org/person/'+ p1.tmdbId }} AS start,
+  p2 {{ .name, .born, link:'https://www.themoviedb.org/person/'+ p2.tmdbId }} AS end,
+  reduce(output = '', i in range(0, length(path)-1) |
+    output + CASE
+      WHEN i = 0 THEN
+       startNode(rels[i]).name + CASE WHEN type(rels[i]) = 'ACTED_IN' THEN ' played '+ rels[i].role +' in 'ELSE ' directed ' END + endNode(rels[i]).title
+       ELSE
+         ' with '+ startNode(rels[i]).name + ', who '+ CASE WHEN type(rels[i]) = 'ACTED_IN' THEN 'played '+ rels[i].role +' in '
+    ELSE 'directed '
+      END + endNode(rels[i]).title
+      END
+  ) AS pathBetweenPeople
+```
+
+Schema:
+{schema}
+
+Question:
+{question}
+"""
 
 CYPHER_GENERATION_TEMPLATE = """
 You are an expert Neo4j Developer translating user questions into Cypher to answer questions about node similarity and provide recommendations.
