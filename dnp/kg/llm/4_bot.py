3faf1448dbce54473205516ec8d94afe24d47917
smallcat9603 <huyao0107@gmail.com>
2024-01-11 22:37:55 +0900

    minor

--- dnp/kg/llm/4_bot.py
+++ dnp/kg/llm/4_bot.py
@@ -63,6 +63,47 @@ graph = Neo4jGraph(
 )
 
 CYPHER_GENERATION_TEMPLATE = """
+You are an expert Neo4j Developer translating user questions into Cypher to answer questions about movies and provide recommendations.
+Convert the user's question based on the schema.
+
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+
+Fine Tuning:
+
+For movie titles that begin with "The", move "the" to the end. For example "The 39 Steps" becomes "39 Steps, The" or "the matrix" becomes "Matrix, The".
+
+Example Cypher Statements:
+
+1. How to find how many degrees of separation there are between two people:
+```
+MATCH path = shortestPath(
+  (p1:Person {{name: "Actor 1"}})-[:ACTED_IN|DIRECTED*]-(p2:Person {{name: "Actor 2"}})
+)
+WITH path, p1, p2, relationships(path) AS rels
+RETURN
+  p1 {{ .name, .born, link:'https://www.themoviedb.org/person/'+ p1.tmdbId }} AS start,
+  p2 {{ .name, .born, link:'https://www.themoviedb.org/person/'+ p2.tmdbId }} AS end,
+  reduce(output = '', i in range(0, length(path)-1) |
+    output + CASE
+      WHEN i = 0 THEN
+       startNode(rels[i]).name + CASE WHEN type(rels[i]) = 'ACTED_IN' THEN ' played '+ rels[i].role +' in 'ELSE ' directed ' END + endNode(rels[i]).title
+       ELSE
+         ' with '+ startNode(rels[i]).name + ', who '+ CASE WHEN type(rels[i]) = 'ACTED_IN' THEN 'played '+ rels[i].role +' in '
+    ELSE 'directed '
+      END + endNode(rels[i]).title
+      END
+  ) AS pathBetweenPeople
+```
+
+Schema:
+{schema}
+
+Question:
+{question}
+"""
+
+CYPHER_GENERATION_TEMPLATE = """
 You are an expert Neo4j Developer translating user questions into Cypher to answer questions about node similarity and provide recommendations.
 Convert the user's question based on the schema.
 
@@ -116,6 +157,13 @@ memory = ConversationBufferWindowMemory(
     k=5,
     return_messages=True,
 )
+SYSTEM_MESSAGE = """
+You are a movie expert providing information about movies.
+Be as helpful as possible and return as much information as possible.
+Do not answer any questions that do not relate to movies, actors or directors.
+
+Do not answer any questions using your pre-trained knowledge, only use the information provided in the context.
+"""
 SYSTEM_MESSAGE = """
 You are a expert providing information about node similariy.
 Be as helpful as possible and return as much information as possible.
