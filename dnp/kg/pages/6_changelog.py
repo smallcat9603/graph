f8fe0fc9ecd9f33dbb67b212ba19283db2d259e2
smallcat9603 <huyao0107@gmail.com>
2024-01-17 15:26:19 +0900

    minor

--- dnp/kg/pages/6_changelog.py
+++ dnp/kg/pages/6_changelog.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.markdown("""
             ## 240117
             - Optimized "Reset" procedure
+            - Confirmed to reset gdbs status when switching between databases
             ## 240116
             - Added chart and changelog pages
 """)
